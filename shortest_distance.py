1bdcbba019bbd55d6cb836822e93c29a9f000402
Joerg Haubrichs <joerghaubrichs@googlemail.com>
2018-09-03 17:09:42 +0200

    Working implementation of find_shortest_distance
    Signed-off-by: Joerg Haubrichs <joerghaubrichs@googlemail.com>

--- shortest_distance.py
+++ shortest_distance.py
@@ -4,4 +4,15 @@ from typing import Union
 def find_shortest_distance(text: str, word1: str, word2: str, 
                            ignore_case: bool=False) -> Union[int, None]:
     
-    return -1
+    flags = re.IGNORECASE if ignore_case else 0
+    
+    pattern = r'(?:^|\W)%s(.{1,}?)%s(?:\W|$)'
+    matches = re.findall(pattern % (word1, word2), text, flags)
+    if not word1 == word2:
+        matches += re.findall(pattern % (word2, word1), text, flags)
+
+    if matches:
+        word_match = re.compile(r'\w+')
+        return min(len(word_match.findall(m)) for m in matches)
+    else:
+        return None
