af99ac3320cbd5798eee5ac533b7593323d6ed8f
Joerg Haubrichs <joerghaubrichs@googlemail.com>
2018-09-03 17:12:13 +0200

    Add documentation
    Signed-off-by: Joerg Haubrichs <joerghaubrichs@googlemail.com>

--- shortest_distance.py
+++ shortest_distance.py
@@ -3,9 +3,27 @@ from typing import Union
 
 def find_shortest_distance(text: str, word1: str, word2: str, 
                            ignore_case: bool=False) -> Union[int, None]:
+    """Shortest word distance calculator
+
+    Finds the shortest distance counted in number of words of two 
+    given words in a text.
+
+    Executes in linear time.
+
+    Args:
+        text (str): The text to search through
+        word1 (str): First word
+        word2 (str): Second word
+        ignore_case (bool): Ignore case of words matched
+
+    Returns:
+        Number of words inbetween. None unless both words are found.
+    """
     
     flags = re.IGNORECASE if ignore_case else 0
     
+    # Find sequences of word1 followed by whatever string up to word2,
+    # and the reverse (from word2 to word1)
     pattern = r'(?:^|\W)%s(.{1,}?)%s(?:\W|$)'
     matches = re.findall(pattern % (word1, word2), text, flags)
     if not word1 == word2:
@@ -13,6 +31,8 @@ def find_shortest_distance(text: str, word1: str, word2: str,
 
     if matches:
         word_match = re.compile(r'\w+')
+        # Count the words found in the matches and return the smallest
         return min(len(word_match.findall(m)) for m in matches)
     else:
         return None
+
